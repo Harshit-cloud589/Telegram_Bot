85f8834fce8e2fa0f3ed8a3c1b97a890428bb627
Harshit <harshvasu07061@gmail.com>
2026-07-31 16:41:25 +0530

    changed model(2)

--- agent/orchestrator.py
+++ agent/orchestrator.py
@@ -11,7 +11,7 @@ from logger import LOG_URL
 
 # client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-MODEL_NAME = "openai/gpt-oss-120b"
+MODEL_NAME = "moonshotai/kimi-k2-instruct"
 import inspect
 import json
 import re
